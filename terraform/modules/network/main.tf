1df10212e0bda69076edc3a1f057c90e4b325bf9
welcomepradeep <pradeep.nayak@hotmail.com>
2026-04-18 21:52:36 +0530

    add file

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,16 +1,16 @@
-resource "null_resource" "network" {
+resource "null_resource" "create_switch" {
 
   triggers = {
-    switch_name = var.switch_name
-    script_hash = filemd5("${path.root}/scripts/create-switch.ps1")
+    switch_name  = var.switch_name
+    adapter_name = var.adapter_name
+    script_hash  = filemd5("${path.root}/scripts/create-switch.ps1")
   }
 
   provisioner "local-exec" {
     command = <<EOT
-      pwsh -ExecutionPolicy Bypass -File "${path.root}/scripts/create-switch.ps1" `
-        -SwitchName "${var.switch_name}" `
-        -AdapterName "${var.adapter_name}" `
-        -AllowManagementOS "${var.allow_management_os}"
+      pwsh -ExecutionPolicy Bypass -File "${path.root}/scripts/create-switch.ps1" \
+        -switchName "${var.switch_name}" \
+        -adapterName "${var.adapter_name}"
     EOT
   }
 }
