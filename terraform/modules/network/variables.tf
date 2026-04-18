9873cddfb6fc37f303fe496b4e50dace10ce6951
welcomepradeep <pradeep.nayak@hotmail.com>
2026-04-18 21:53:21 +0530

    add file

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -7,9 +7,3 @@ variable "adapter_name" {
   description = "Physical adapter name"
   type        = string
 }
-
-variable "allow_management_os" {
-  description = "Allow management OS to share adapter"
-  type        = bool
-  default     = true
-}
