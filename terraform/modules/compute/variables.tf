d7dc10e4c9042117d7543ed29130cdbe4bfceb62
welcomepradeep <pradeep.nayak@hotmail.com>
2026-04-18 16:41:32 +0530

    add file

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -1,8 +1,10 @@
 variable "rac1_name" {}
 variable "rac2_name" {}
 
-variable "memory" {}
+variable "memory" {
+  type = number
+}
+
 variable "vhd_path" {}
 
 variable "public_switch" {}
-variable "private_switch" {}
