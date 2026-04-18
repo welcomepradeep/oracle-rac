f5bf6650fd1bf60a8a89851f52331ef32c3a0911
welcomepradeep <pradeep.nayak@hotmail.com>
2026-04-18 09:56:58 +0530

    fill delete

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,5 +1,12 @@
 resource "null_resource" "network" {
+
+  triggers = {
+    script_hash = filemd5("${path.root}/scripts/create-switch.ps1")
+  }
+
   provisioner "local-exec" {
-    command = "powershell -ExecutionPolicy Bypass -File ../../scripts/create-switch.ps1"
+    command = <<EOT
+      pwsh -ExecutionPolicy Bypass -File "${path.root}/scripts/create-switch.ps1"
+    EOT
   }
 }
