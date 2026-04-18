a2dd35f0477cdc25ade823b6a930ab58dcf0f2f4
welcomepradeep <pradeep.nayak@hotmail.com>
2026-04-18 16:54:50 +0530

    add file

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,11 +1,22 @@
 module "compute" {
   source = "./modules/compute"
 
-  rac1_name      = "rac1"
-  rac2_name      = "rac2"
-  memory         = 8192
-  vhd_path       = "C:\\HyperV\\base.vhdx"
-  public_switch  = module.network.switch_name
+  vms = {
+    rac1 = {
+      memory      = 8192
+      cpu         = 2
+      vhd_path    = "C:\\HyperV\\rac1.vhdx"
+      switch_name = module.network.switch_name
+    }
+
+    rac2 = {
+      memory      = 8192
+      cpu         = 2
+      vhd_path    = "C:\\HyperV\\rac2.vhdx"
+      switch_name = module.network.switch_name
+    }
+
+  }
 
   depends_on = [
     module.network,
