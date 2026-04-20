266ce5118f60d7bb004116d448963b4a013288b0
welcomepradeep <pradeep.nayak@hotmail.com>
2026-04-20 13:13:49 +0530

    sync

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,9 +1,18 @@
 resource "null_resource" "vm" {
   for_each = var.vms
 
+  triggers = {
+    vm_name = each.key
+  }
+
   provisioner "remote-exec" {
     inline = [
-      "powershell -ExecutionPolicy Bypass -File C:\\scripts\\create-vm.ps1 ..."
+      "powershell -ExecutionPolicy Bypass -File C:\\scripts\\create-vm.ps1 " 
+      + "-vm_name ${each.key} "
+      + "-memory ${each.value.memory} "
+      + "-cpu ${each.value.cpu} "
+      + "-vhd_path ${each.value.vhd_path} "
+      + "-switch_name ${each.value.switch_name}"
     ]
 
     connection {
