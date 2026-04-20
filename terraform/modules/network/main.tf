ff0b59d38688030080b7614cadd02e25870c6909
welcomepradeep <pradeep.nayak@hotmail.com>
2026-04-20 22:59:36 +0530

    modified

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,16 +1,5 @@
-resource "null_resource" "create_switch" {
-
-  triggers = {
-    switch_name  = var.switch_name
-    adapter_name = var.adapter_name
-    script_hash  = filemd5("${path.root}/scripts/create-switch.ps1")
-  }
-
+resource "null_resource" "network" {
   provisioner "local-exec" {
-    command = <<EOT
-      pwsh -ExecutionPolicy Bypass -File "${path.root}/scripts/create-switch.ps1" \
-        -switchName "${var.switch_name}" \
-        -adapterName "${var.adapter_name}"
-    EOT
+    command = "powershell -ExecutionPolicy Bypass -File ../../scripts/create-switch.ps1"
   }
 }
