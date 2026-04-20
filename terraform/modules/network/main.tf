4eecb816f62960a3f99cdc596e9544d772dd8833
welcomepradeep <pradeep.nayak@hotmail.com>
2026-04-20 23:03:30 +0530

    modified

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,5 +1,17 @@
-resource "null_resource" "network" {
-  provisioner "local-exec" {
-    command = "powershell -ExecutionPolicy Bypass -File ../../scripts/create-switch.ps1"
+resource "null_resource" "create_switch" {
+
+  provisioner "remote-exec" {
+    inline = [
+      "powershell -ExecutionPolicy Bypass -File C:\\scripts\\create-switch.ps1 -switchName ${var.switch_name} -adapterName ${var.adapter_name}"
+    ]
+
+    connection {
+      type     = "winrm"
+      host     = var.hyperv_host
+      user     = var.hyperv_user
+      password = var.hyperv_password
+      https    = false
+      insecure = true
+    }
   }
 }
