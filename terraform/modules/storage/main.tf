48d1683daba5bb6d365eb29f1a8e7d6517478c13
welcomepradeep <pradeep.nayak@hotmail.com>
2026-04-21 23:16:24 +0530

    modified

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -1,17 +1,5 @@
-resource "null_resource" "storage" {
-
-  triggers = {
-    disk_name   = var.disk_name
-    disk_size   = var.disk_size_gb
-    script_hash = filemd5("${path.module}/scripts/create-disk.ps1")
-  }
-
+resource "null_resource" "shared_disks" {
   provisioner "local-exec" {
-    command = <<EOT
-      pwsh -ExecutionPolicy Bypass -File "${path.module}/scripts/create-disk.ps1" `
-        -DiskName "${var.disk_name}" `
-        -DiskPath "${var.disk_path}" `
-        -DiskSizeGB ${var.disk_size_gb}
-    EOT
+    command = "powershell -ExecutionPolicy Bypass -File ../../scripts/create-disk.ps1"
   }
 }
