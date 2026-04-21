a0c6758d1748d14523ed811d4dd37b00ab4e9a60
welcomepradeep <pradeep.nayak@hotmail.com>
2026-04-21 23:04:02 +0530

    modified

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "storage" {
   triggers = {
     disk_name   = var.disk_name
     disk_size   = var.disk_size_gb
-    script_hash = filemd5("${path.module}/../../scripts/create-disk.ps1")
+    script_hash = filemd5("${path.module}/scripts/create-disk.ps1")
   }
 
   provisioner "local-exec" {
