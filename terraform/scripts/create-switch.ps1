557f3b060327b62416c8f63d17622311c8098ee8
welcomepradeep <pradeep.nayak@hotmail.com>
2026-04-25 19:12:09 +0530

    add file

--- terraform/scripts/create-switch.ps1
+++ terraform/scripts/create-switch.ps1
@@ -1,2 +1,11 @@
-New-VMSwitch -Name "ExternalSwitch" -NetAdapterName "Ethernet" -AllowManagementOS $true -ErrorAction SilentlyContinue
-New-VMSwitch -Name "RAC-Private" -SwitchType Internal -ErrorAction SilentlyContinue
+# Create switches
+New-VMSwitch -Name "ExternalSwitch" -NetAdapterName "Realtek 8852CE WiFi 6E PCI-E NIC" -AllowManagementOS $true
+New-VMSwitch -Name "RAC-Private" -SwitchType Internal
+
+# Create VMs
+New-VM -Name "rac1" -MemoryStartupBytes 8GB -SwitchName "ExternalSwitch"
+New-VM -Name "rac2" -MemoryStartupBytes 8GB -SwitchName "ExternalSwitch"
+
+# Add private network
+Add-VMNetworkAdapter -VMName "rac1" -SwitchName "RAC-Private"
+Add-VMNetworkAdapter -VMName "rac2" -SwitchName "RAC-Private"
