0cf4b0b85c4becf41e5b6341eefd652263d738ef
welcomepradeep <pradeep.nayak@hotmail.com>
2026-04-25 19:04:21 +0530

    add file

--- terraform/scripts/create-disk.ps1
+++ terraform/scripts/create-disk.ps1
@@ -1,2 +1,4 @@
 New-VHD -Path "C:\HyperV\Disks\ocr.vhdx" -SizeBytes 10GB -Dynamic
 New-VHD -Path "C:\HyperV\Disks\data.vhdx" -SizeBytes 10GB -Dynamic
+New-VHD -Path "C:\HyperV\Disks\rac1.vhdx" -SizeBytes 30GB -Dynamic
+New-VHD -Path "C:\HyperV\Disks\rac2.vhdx" -SizeBytes 30GB -Dynamic
