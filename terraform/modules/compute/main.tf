ef2144ed047244de230c3362a3675327ae5d3bd6
welcomepradeep <pradeep.nayak@hotmail.com>
2026-04-26 20:50:55 +0530

    files updated

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -41,19 +41,11 @@ ${var.hyperv_user}@${var.hyperv_host}:/C:/Terraform/scripts/create-vm.ps1
 # ----------------------------------------
 # Step 3 - Execute script remotely
 # ----------------------------------------
-sshpass -p '${var.hyperv_password}' ssh \
--o StrictHostKeyChecking=no \
--o PreferredAuthentications=password \
--o PubkeyAuthentication=no \
-${var.hyperv_user}@${var.hyperv_host} \
-"powershell -ExecutionPolicy Bypass -File C:\\Terraform\\scripts\\create-vm.ps1 \
--vmName '${each.key}' \
--memory ${each.value.memory} \
--vhdPath '${each.value.vhd_path}' \
--switchName '${each.value.switch_name}' \
--isoPath '${each.value.iso_path}' \
--cpu ${each.value.cpu} \
--privateSwitch '${var.private_switch}'"
+sshpass -p '${var.hyperv_password}' ssh -o StrictHostKeyChecking=no ${var.hyperv_user}@${var.hyperv_host} "powershell -Command \"New-Item -ItemType Directory -Force -Path C:\\Terraform\\scripts | Out-Null\""
+
+sshpass -p '${var.hyperv_password}' scp -o StrictHostKeyChecking=no ${path.root}/scripts/create-vm.ps1 ${var.hyperv_user}@${var.hyperv_host}:/C:/Terraform/scripts/create-vm.ps1
+
+sshpass -p '${var.hyperv_password}' ssh -o StrictHostKeyChecking=no ${var.hyperv_user}@${var.hyperv_host} "powershell -ExecutionPolicy Bypass -File C:\\Terraform\\scripts\\create-vm.ps1 -vmName ${each.key} -memory ${each.value.memory} -vhdPath ${each.value.vhd_path} -switchName ${each.value.switch_name} -isoPath ${each.value.iso_path} -cpu ${each.value.cpu} -privateSwitch ${var.private_switch}"
 EOT
   }
 }
